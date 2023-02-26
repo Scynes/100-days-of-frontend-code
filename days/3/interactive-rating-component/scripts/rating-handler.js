c5cd9164f58c0e57716fc953768b6c94bf10e6a9
Scynes <dustin.greyfield@gmail.com>
2023-02-25 21:29:39 -0500

    day-3: added submission functionality

--- days/3/interactive-rating-component/scripts/rating-handler.js
+++ days/3/interactive-rating-component/scripts/rating-handler.js
@@ -1,5 +1,3 @@
-let currentRating = 5;
-
 let previousSelection = null;
 
 const RATING_OPTIONS = document.querySelectorAll('.rating-option');
@@ -22,6 +20,17 @@ const handleRatingSelect = event => {
 }
 
 const handleSubmit = () => {
+    
+    // Verify that a selection was made
+    if (previousSelection === null) return;
+
+    const SELECTED_DISPLAY = document.querySelector('.selected-display');
+    const RATING_THANKS = document.querySelector('.rating-thanks');
+    const RATING_SUBMIT = document.querySelector('.rating-submit');
+
+    SELECTED_DISPLAY.textContent = `You selected ${previousSelection.textContent} out of 5`;
+    RATING_SUBMIT.classList.toggle('hidden');
+    RATING_THANKS.classList.toggle('hidden');
 
 }
 
