28e67152c8b7bff48f088083b128067437d17cb2
Scynes <dustin.greyfield@gmail.com>
2023-03-01 16:16:55 -0500

    day-7: added input functionality for setting tip, bill and people inputs

--- days/7/tip-splitter/scripts/bind-listeners.js
+++ days/7/tip-splitter/scripts/bind-listeners.js
@@ -1,5 +1,20 @@
-import { validateInputAsNumber } from "./inputs-validator.js";
+import { validateInputAsNumber, validateInputAsDollarAmount, setCustomTipPercentage, setTipPercentageWithButton } from "./inputs-validator.js";
 
 const NUMBER_OF_PEOPLE_INPUT = document.getElementById('people');
 
-NUMBER_OF_PEOPLE_INPUT.addEventListener('change', validateInputAsNumber);
+const BILL_AMOUNT_INPUT = document.getElementById('bill');
+
+const TIP_BUTTONS = document.querySelectorAll('.button-container button');
+
+const CUSTOM_TIP = document.getElementById('custom');
+
+NUMBER_OF_PEOPLE_INPUT.addEventListener('keyup', validateInputAsNumber);
+
+BILL_AMOUNT_INPUT.addEventListener('keyup', validateInputAsDollarAmount);
+
+CUSTOM_TIP.addEventListener('keyup', setCustomTipPercentage);
+
+TIP_BUTTONS.forEach(button => {
+
+    button.addEventListener('click', setTipPercentageWithButton);
+})
--- days/7/tip-splitter/scripts/inputs-validator.js
+++ days/7/tip-splitter/scripts/inputs-validator.js
@@ -1,3 +1,9 @@
+let validDollarInput = 0;
+
+let validNumberOfPeople = 0;
+
+let currentTipPercent = 0;
+
 export const validateInputAsNumber = event => {
     event.preventDefault();
 
@@ -6,11 +12,42 @@ export const validateInputAsNumber = event => {
 
     if (INPUT_VALUE > 0 && !isNaN(INPUT_VALUE)) {
         event.target.value = INPUT_VALUE;
+        validNumberOfPeople = parseInt(INPUT_VALUE);
         event.target.style.outline = 'none'
         LABEL_ERROR.style.display = 'none';
     } else {
         LABEL_ERROR.style.display = 'inline-block';
         event.target.style.outline = '2px solid red'
         event.target.value = 0;
+    }
+}
+
+export const validateInputAsDollarAmount = event => {
+    event.preventDefault();
+
+    const INPUT_VALUE = event.target.value;
+
+    if (/^\d+(\.\d{0,2})?$/.test(INPUT_VALUE)) {
+        validDollarInput = parseFloat(INPUT_VALUE);
+    } else {
+        event.target.value = validDollarInput;
+    }
+}
+
+export const setTipPercentageWithButton = event => {
+
+    const PERCENT_VALUE = parseInt(event.target.textContent) / 100;
+
+    currentTipPercent = PERCENT_VALUE;
+}
+
+export const setCustomTipPercentage = event => {
+
+    const INPUT_VALUE = event.target.value;
+
+    if (INPUT_VALUE > 0 && !isNaN(INPUT_VALUE)) {
+        currentTipPercent = parseInt(INPUT_VALUE) / 100;
+    } else {
+        event.target.value = currentTipPercent;
     }
 }
