c06e35a41c3eeecfc027dbfde17a6bcdce84022d
Scynes <dustin.greyfield@gmail.com>
2023-03-10 12:57:53 -0500

    day-12: added modal toggling

---
+++ days/12/pomodoro-timer/scripts/settings.js
@@ -0,0 +1,13 @@
+const openSettings = () => {
+
+    const MODAL = document.querySelector('.settings-modal-container');
+
+    MODAL.style.display = 'block';
+}
+
+const applySettings = () => {
+
+    const MODAL = document.querySelector('.settings-modal-container');
+
+    MODAL.style.display = 'none';
+}
